7c7a57dfa59c3896c48452f86d92c1769380dbcc
Tech-Murefu <sean.s.murefu@gmail.com>
2025-10-03 15:35:34 +0200

    feat: set up api for signup and authenticaion

--- airport-passenger-flow-delay-prediction/backend/airport_api/urls.py
+++ airport-passenger-flow-delay-prediction/backend/airport_api/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from core import views
 from django.http import JsonResponse
 
 def root_view(request):
@@ -17,8 +19,14 @@ def health_check_view(request):
     return JsonResponse({"status": "ok"})
 
 urlpatterns = [
-    path("", root_view),
+    path("", views.welcome_view),   # Public Welcome Page
     path("admin/", admin.site.urls),
+
+    # API routes
     path("api/", include("core.urls")),
     path("health/", health_check_view),
+
+    # JWT Authentication
+    path("api/token/", TokenObtainPaireView.as_view(), name="oken_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
