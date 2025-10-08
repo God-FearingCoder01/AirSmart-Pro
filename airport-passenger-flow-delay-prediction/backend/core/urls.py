b19b72a51b7f8672cf06a7547fd09787a1dc9d75
Tech-Murefu <sean.s.murefu@gmail.com>
2025-10-08 22:16:54 +0200

    chore: added urls

--- airport-passenger-flow-delay-prediction/backend/core/urls.py
+++ airport-passenger-flow-delay-prediction/backend/core/urls.py
@@ -3,8 +3,11 @@ from .views import (
     DashboardStatsView,
     FlightDelayPredictionView,
     PassengerFlowView,
+    SignUpView,
+    CustomLoginView
 )
 from django.http import JsonResponse 
+from rest_framework_simplejwt.views import TokenRefreshView
 
 def api_root_view(request):
     return JsonResponse({
@@ -21,4 +24,7 @@ urlpatterns = [
     path("dashboard-stats/", DashboardStatsView.as_view(), name="dashboard-stats"),
     path("delay-predictions/", FlightDelayPredictionView.as_view(), name="delay-predictions"),
     path("passenger-flow/", PassengerFlowView.as_view(), name="passenger-flow"),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
